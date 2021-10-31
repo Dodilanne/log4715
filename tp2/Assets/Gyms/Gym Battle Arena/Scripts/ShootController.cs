1bfceddc4465dd210fc16c29e72e3925d1e41fbc
Vincent Audet <vincaudet@gmail.com>
2021-10-31 17:00:43 -0400

    Add shooting animation

---
+++ tp2/Assets/Gyms/Gym Battle Arena/Scripts/ShootController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ShootController : MonoBehaviour {
+  // Serialized attributes
+  [SerializeField] float PickupAnimSpeed = 6f;
+  [SerializeField] int ShotCount;
+
+  // Private attributes
+  private bool _Grounded { get; set; }
+  private Animator _Anim { get; set; }
+  private bool _CanShoot;
+  private float _ShotDuration = -1f;
+
+  void Awake() {
+    _Anim = GetComponent<Animator>();
+  }
+
+  void Start() {
+    ShotCount = 0;
+    _CanShoot = true;
+    SetShotDuration();
+  }
+
+
+  private void Update() {
+    if (_CanShoot && Input.GetButtonDown("Fire1")) {
+      StartCoroutine(Shoot());
+    }
+  }
+
+  private IEnumerator Shoot() {
+    if (_ShotDuration < 0) yield return false;
+
+    _CanShoot = false;
+    _Anim.SetTrigger("Pickup");
+    yield return new WaitForSeconds(_ShotDuration);
+    _CanShoot = true;
+  }
+
+  private void SetShotDuration() {
+    IEnumerable<AnimationClip> clips = _Anim.runtimeAnimatorController.animationClips.Where(clip => clip.name == "pickup");
+    if (clips.Count() > 0) {
+      _ShotDuration = clips.First().length / PickupAnimSpeed;
+
+    }
+  }
+}
--- tp2/Assets/Gyms/Gym Dash/DashController.cs
+++ tp2/Assets/Gyms/Gym Dash/DashController.cs
@@ -2,24 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DashController : MonoBehaviour
-{
-    Rigidbody rb;
-    bool dashing = false;
+public class DashController : MonoBehaviour {
+  Rigidbody rb;
+  bool dashing = false;
 
-    [SerializeField]
-    string dashInputAction = "Dash";
+  [SerializeField]
+  string dashInputAction = "Dash";
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
+  // Start is called before the first frame update
+  void Start() {
+    rb = GetComponent<Rigidbody>();
 
-    }
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  // Update is called once per frame
+  void Update() {
+
+  }
 }
