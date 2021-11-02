42fe4c189129d7894d793d050269cd432d4a453e
Rochana Fardon <rochana.fardon@polymtl.ca>
2021-11-02 09:30:01 -0400

    lock and key done

--- tp2/Assets/Gym Lock and key/LockAndKeyControler.cs
+++ tp2/Assets/Gym Lock and key/LockAndKeyControler.cs
@@ -10,11 +10,19 @@ public class LockAndKeyControler : MonoBehaviour
     [SerializeField]
     Text keyText;
 
+    [SerializeField]
+    Text noKeyText;
+
     private int keyNumber = 0;
+    private bool showNoKeyWarning = false;
+    private bool startCoroutine = false;
+    private bool touchedDoorWithKey = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        noKeyText.text = "You need a key!";
+        noKeyText.gameObject.SetActive(false);
         keyText.text = "Key number: " + keyNumber;
     }
 
@@ -22,20 +30,31 @@ public class LockAndKeyControler : MonoBehaviour
     void Update()
     {
       keyText.text = "Key number: " + keyNumber;
+      if(showNoKeyWarning) 
+      {
+        noKeyText.gameObject.SetActive(true);
+        StartCoroutine(ShowWarning());
+        showNoKeyWarning = false;
+      }
+      if(touchedDoorWithKey) 
+      {
+        keyNumber--;
+        touchedDoorWithKey = false;
+      }
     }
 
     void OnCollisionEnter(Collision coll) 
     {
-      // Collision avec la porte
       if (coll.gameObject.layer== 3) 
       {
-        Debug.Log("hit door");
-        if (keyNumber >=1) {
-          keyNumber--;
+        if (keyNumber >=1) 
+        {
           coll.gameObject.transform.Rotate(0,0,-90);
+          touchedDoorWithKey = true;
         }
-        else {
-          // TODO popup "no key"
+        else 
+        {
+          showNoKeyWarning = true;
         }
       }
     }
@@ -46,7 +65,12 @@ public class LockAndKeyControler : MonoBehaviour
       {
         GameObject.Destroy(other.gameObject);
         keyNumber ++;
-        
       }
+    }
+
+    private IEnumerator ShowWarning()
+    {
+      yield return new WaitForSeconds(1);
+      noKeyText.gameObject.SetActive(false);
     }
 }
